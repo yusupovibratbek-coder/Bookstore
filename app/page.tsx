a1241b244cdbebcf0204697126a032d7fde51714
yusupovibratbek-coder <yusupovibratbek18@gmail.com>
2026-04-07 03:28:02 -0700

    feat: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,32 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+  return(
+    <div className="bg-white h-full w-full px-48">
+      {/* navbar */}
+      <div className="h-14 w-full flex items-center font-mono font-bold justify-between">
+        <img className="h-14" src="https://www.creativefabrica.com/wp-content/uploads/2023/06/14/Open-book-icon-Reading-club-logo-Knowl-Graphics-72117356-1.png" alt="" />
+        <div className="flex text-black justify-around w-96">
+          <h1>Main</h1>
+          <h1>Books</h1>
+          <h1>Popular</h1>
+          <h1>Rating</h1>
+          <h1>Contact</h1>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
+
+      {/* hero */}
+      <div className="w-full h-136 bg-red-300"></div>
+
+      {/* features */}
+      <div className="w-full h-96 bg-yellow-300"></div>
+
+      {/* kards */}
+      <div className="w-full h-96 bg-green-300"></div>
+
+      {/* qoshimcha */}
+      <div className="w-full h-96 bg-purple-300"></div>
+
+      {/* footer */}
+      <div className="w-full h-74 bg-gray-800"></div>
     </div>
-  );
+  )
 }
