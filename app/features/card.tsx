3c92bcd135f8420da06dae9bab1bfd3f77a341ce
yusupovibratbek-coder <yusupovibratbek18@gmail.com>
2026-04-08 10:08:40 -0700

    yangilandi'

---
+++ app/features/card.tsx
@@ -0,0 +1,19 @@
+export default function Card() {
+    return (
+        <div className="p-4 ">
+            <div className="flex items-center my-4 border-2 rounded-2xl shadow-lg h-full w-full mb-4">
+                <div className="pl-20 ">
+                    <h1 className="text-4xl font-bold font-mono p-4">Features</h1>
+                <h1 className="w-125 font-mono font-bold p-2 ">Wide Collection of Historical Books. Explore books from ancient civilizations to modern history.</h1>
+                <h1 className="w-120  font-mono font-bold p-2 ">Authentic & Rare Editions. Discover unique and hard-to-find historical publications.</h1>
+                <h1 className="w-120  font-mono font-bold p-2 ">Expert Recommendations. Get suggestions from history enthusiasts and experts.</h1>
+                <h1 className="w-120  font-mono font-bold p-2 ">Comfortable Reading Experience. Enjoy a user-friendly platform designed for book lovers.</h1>
+                <h1 className="w-120  font-mono font-bold p-2 ">Fast & Reliable Delivery. Receive your favorite books quickly and safely.</h1>
+                </div>
+                <div className="pl-50 ">
+                    <img className="w-55" src="https://static.vecteezy.com/system/resources/previews/004/753/005/original/features-icon-shadowed-detailed-features-logo-free-vector.jpg" alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,42 @@
+import { Button } from "@/components/ui/button";
+import Card from "./features/card";
+
 export default function Home() {
-  return(
+  return (
     <div className="bg-white h-full w-full px-48">
       {/* navbar */}
-      <div className="h-14 w-full flex items-center font-mono font-bold justify-between">
-        <img className="h-14" src="https://www.creativefabrica.com/wp-content/uploads/2023/06/14/Open-book-icon-Reading-club-logo-Knowl-Graphics-72117356-1.png" alt="" />
-        <div className="flex text-black justify-around w-96">
+      <div className="h-14 w-full flex items-center font-mono font-bold my-4 border-2 rounded-2xl justify-between  px-4 shadow-lg">
+        <img className="h-14" src="https://i.pinimg.com/originals/e8/2e/c4/e82ec4007494891eac542ac464b9ec30.png" alt="" />
+        
+        <div>
+          <input className="border-1 rounded-4xl p-1 pl-3 " placeholder="Search books" type="text" />
+        </div>
+        <div className="flex text-black justify-around w-96 text-shadow-lg">
           <h1>Main</h1>
           <h1>Books</h1>
           <h1>Popular</h1>
           <h1>Rating</h1>
           <h1>Contact</h1>
         </div>
+        <Button>Log in</Button>
       </div>
 
       {/* hero */}
-      <div className="w-full h-136 bg-red-300"></div>
+      <div className="w-full h-136  flex items-center justify-between border-2 rounded-2xl shadow-lg mb-4">
+        <div className="w-full h-136 flex flex-col items-start justify-center pl-18">
+          <h1 className="text-4xl font-bold font-mono text-gray-800">World of</h1>
+          <h1 className="text-9xl font-black font-mono bg-gradient-to-br from-red-700 to-gray-600 bg-clip-text text-transparent">Books</h1>
+          <p className="text-sm font-mono">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit deserunt ipsum ducimus, nobis minima voluptate voluptatum aperiam quaerat voluptatem culpa at officiis itaque ea mollitia provident labore est maiores magni?</p>
+        </div>
+        <div className="w-full h-136 flex items-center justify-center">
+          <img className="h-112" src="https://upload.wikimedia.org/wikipedia/commons/d/d2/Minha_Luta_-_Mein_Kampf_-_Adolf_Hitler.jpg" alt="" />
+        </div>
+      </div>
 
       {/* features */}
-      <div className="w-full h-96 bg-yellow-300"></div>
+      <div className="w-full h-112 ">
+        <Card></Card>
+      </div>
 
       {/* kards */}
       <div className="w-full h-96 bg-green-300"></div>
